c0251c592dddee9e75a3702244d58fa6586411cc
Quoc-Viet Nguyen <afelion@gmail.com>
2012-10-09 18:56:35 +1000

    Add Compass view and service

---
+++ SolarPowerCalc_Android/src/com/qut/spc/service/CompassService.java
@@ -0,0 +1,73 @@
+package com.qut.spc.service;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class CompassService implements SensorEventListener {
+	private float[] accValues = new float[3];
+	private float[] magValues = new float[3];
+	
+	private float[] orientation = new float[3];
+	
+	private SensorManager manager;
+	
+	public CompassService(Context ctx) {
+		// SensorManager instance
+		manager = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+
+		registerListener();
+	}
+
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// Do nothing
+	}
+
+	public void onSensorChanged(SensorEvent e) {
+		// Gets the value of the sensor that has been changed
+		switch (e.sensor.getType()) {
+		case Sensor.TYPE_ACCELEROMETER:
+			accValues = e.values.clone();
+			break;
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			magValues = e.values.clone();
+			break;
+		}
+		calculateOrientation();
+	}
+	
+	public float[] getOrientation() {
+		return orientation;
+	}
+	
+	public void registerListener() {
+		Sensor accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Sensor magnetfield = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+		// Register the listener
+		manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		manager.registerListener(this, magnetfield, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	public void unregisterListener() {
+		manager.unregisterListener(this);
+	}
+	
+	private void calculateOrientation() {
+		float[] R = new float[9];
+		float[] outR = new float[9];
+
+		SensorManager.getRotationMatrix(R, null, accValues, magValues);
+		SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_X,
+				SensorManager.AXIS_Z, outR);
+
+		SensorManager.getOrientation(outR, orientation);
+
+		// Convert from Radians to Degrees.
+		orientation[0] = (float) Math.toDegrees(orientation[0]);
+		orientation[1] = (float) Math.toDegrees(orientation[1]);
+		orientation[2] = (float) Math.toDegrees(orientation[2]);
+	}
+}
---
+++ SolarPowerCalc_Android/src/com/qut/spc/view/CompassView.java
@@ -0,0 +1,215 @@
+package com.qut.spc.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @see Professional Android 2 Application Development, chap 14
+ */
+public class CompassView extends View {
+
+	private Paint markerPaint;
+	private Paint textPaint;
+	private Paint circlePaint;
+	private String northString;
+	private String eastString;
+	private String southString;
+	private String westString;
+	private int textHeight;
+
+	private float bearing;
+	float pitch = 0;
+	float roll = 0;
+
+	public void setBearing(float _bearing) {
+		bearing = _bearing;
+	}
+
+	public float getBearing() {
+		return bearing;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public void setPitch(float pitch) {
+		this.pitch = pitch;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+
+	public void setRoll(float roll) {
+		this.roll = roll;
+	}
+
+	public CompassView(Context context) {
+		super(context);
+		initCompassView();
+	}
+
+	public CompassView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initCompassView();
+	}
+
+	public CompassView(Context context, AttributeSet ats, int defaultStyle) {
+		super(context, ats, defaultStyle);
+		initCompassView();
+	}
+
+	protected void initCompassView() {
+		setFocusable(true);
+		
+		int bgColor = Color.rgb(255, 160, 0);
+		int fgColor = Color.BLACK;
+
+		circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		circlePaint.setColor(bgColor);
+		circlePaint.setStrokeWidth(1);
+		circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+		northString = "N";
+		eastString = "E";
+		southString = "S";
+		westString = "W";
+
+		textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		textPaint.setColor(fgColor);
+
+		textHeight = (int) textPaint.measureText("yY");
+
+		markerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		markerPaint.setColor(fgColor);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// The compass is a circle that fills as much space as possible.
+		// Set the measured dimensions by figuring out the shortest boundary,
+		// height or width.
+		int measuredWidth = measure(widthMeasureSpec);
+		int measuredHeight = measure(heightMeasureSpec);
+
+		int d = Math.min(measuredWidth, measuredHeight);
+
+		setMeasuredDimension(d, d);
+	}
+
+	private int measure(int measureSpec) {
+		int result = 0;
+
+		// Decode the measurement specifications.
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
+
+		if (specMode == MeasureSpec.UNSPECIFIED) {
+			// Return a default size of 200 if no bounds are specified.
+			result = 200;
+		} else {
+			// As you want to fill the available space
+			// always return the full available bounds.
+			result = specSize;
+		}
+		return result;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		int mMeasuredWidth = getMeasuredWidth();
+		int mMeasuredHeight = getMeasuredWidth();
+
+		int px = getMeasuredWidth() / 2;
+		int py = getMeasuredHeight() / 2;
+
+		int radius = Math.min(px, py);
+		// Draw the background
+		canvas.drawCircle(px, py, radius, circlePaint);
+		// Rotate our perspective so that the ‘top’ is
+		// facing the current bearing.
+		canvas.save();
+		canvas.rotate(-bearing, px, py);
+		int textWidth = (int) textPaint.measureText("W");
+		int cardinalX = px - textWidth / 2;
+		int cardinalY = py - radius + textHeight;
+
+		// Draw the marker every 15 degrees and text every 45.
+		for (int i = 0; i < 24; i++) {
+			// Draw a marker.
+			canvas.drawLine(px, py - radius, px, py - radius + 10, markerPaint);
+
+			canvas.save();
+			canvas.translate(0, textHeight);
+
+			// Draw the cardinal points
+			if (i % 6 == 0) {
+				String dirString = "";
+				switch (i) {
+				case (0): {
+					dirString = northString;
+					int arrowY = 2 * textHeight;
+					canvas.drawLine(px, arrowY, px - 5, 3 * textHeight,
+							markerPaint);
+					canvas.drawLine(px, arrowY, px + 5, 3 * textHeight,
+							markerPaint);
+					break;
+				}
+				case (6):
+					dirString = eastString;
+					break;
+				case (12):
+					dirString = southString;
+					break;
+				case (18):
+					dirString = westString;
+					break;
+				}
+				canvas.drawText(dirString, cardinalX, cardinalY, textPaint);
+			}
+
+			else if (i % 3 == 0) {
+				// Draw the text every alternate 45deg
+				String angle = String.valueOf(i * 15);
+				float angleTextWidth = textPaint.measureText(angle);
+
+				int angleTextX = (int) (px - angleTextWidth / 2);
+				int angleTextY = py - radius + textHeight;
+				canvas.drawText(angle, angleTextX, angleTextY, textPaint);
+			}
+			canvas.restore();
+
+			canvas.rotate(15, px, py);
+		}
+		canvas.restore();
+
+		RectF rollOval = new RectF((mMeasuredWidth / 3) - mMeasuredWidth / 7,
+				(mMeasuredHeight / 2) - mMeasuredWidth / 7,
+				(mMeasuredWidth / 3) + mMeasuredWidth / 7,
+				(mMeasuredHeight / 2) + mMeasuredWidth / 7);
+		markerPaint.setStyle(Paint.Style.STROKE);
+		canvas.drawOval(rollOval, markerPaint);
+		markerPaint.setStyle(Paint.Style.FILL);
+		canvas.save();
+		canvas.rotate(roll, mMeasuredWidth / 3, mMeasuredHeight / 2);
+		canvas.drawArc(rollOval, 0, 180, false, markerPaint);
+
+		canvas.restore();
+		RectF pitchOval = new RectF((2 * mMeasuredWidth / 3) - mMeasuredWidth
+				/ 7, (mMeasuredHeight / 2) - mMeasuredWidth / 7,
+				(2 * mMeasuredWidth / 3) + mMeasuredWidth / 7,
+				(mMeasuredHeight / 2) + mMeasuredWidth / 7);
+		markerPaint.setStyle(Paint.Style.STROKE);
+		canvas.drawOval(pitchOval, markerPaint);
+		markerPaint.setStyle(Paint.Style.FILL);
+		canvas.drawArc(pitchOval, 0 - pitch / 2, 180 + (pitch), false,
+				markerPaint);
+		markerPaint.setStyle(Paint.Style.STROKE);
+	}
+}
