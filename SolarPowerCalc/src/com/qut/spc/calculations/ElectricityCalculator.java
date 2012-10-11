1bb8162b884c637f24ef0b1c44779e5cff445bee
Alex <pingshuibing@gmail.com>
2012-10-12 02:37:45 +1000

    modify electricityCalculator
    change solarpanelEfficiency to ageEfficiencyLoss, oringal data of
    solarpanelEfficiency become 1.

--- SolarPowerCalc/src/com/qut/spc/calculations/ElectricityCalculator.java
+++ SolarPowerCalc/src/com/qut/spc/calculations/ElectricityCalculator.java
@@ -11,21 +11,22 @@ public class ElectricityCalculator implements ElectricityCalculationApi{
 	
 	//from google docs
 	private static final double CORRECTION_FACTOR = 1.25;
-    private static final double PANEL_AGE_EFFICIENCY_LOSS = 0.007;
+    //private static final double PANEL_AGE_EFFICIENCY_LOSS = 0.007;
+	
 	@Override
 	public double getElectricityProduction(double dailySun,
-			double inverterEfficiency, double solarPanelEfficiency,
+			double inverterEfficiency, double ageEfficiencyLoss,
 			double solarPowerOutput,double dailyHours, double timespan) throws IllegalArgumentException {
 		
 		//formula: (  estimated watt need/timespan*1.25)*(invertefficiency) 
 		//assume that capacity(solarPowerOutput) is peak electricity generation per hour
 		
-		restrictInput(dailySun,inverterEfficiency, solarPanelEfficiency,solarPowerOutput,dailyHours,timespan);
+		restrictInput(dailySun,inverterEfficiency, ageEfficiencyLoss,solarPowerOutput,dailyHours,timespan);
 		int timespanYear = (int)timespan/365; //get how many timespan user want to calculate
 		
 		double electricity = 0;
 		for (int i = 0; i < (timespanYear + 1); i ++) {
-			double actualSunPower = (dailySun/CORRECTION_FACTOR)*(solarPanelEfficiency-(PANEL_AGE_EFFICIENCY_LOSS*i)); //a sunlight correction factor of southern hemisphere
+			double actualSunPower = (dailySun/CORRECTION_FACTOR)*(1-(ageEfficiencyLoss*i)); //a sunlight correction factor of southern hemisphere
 			if (solarPowerOutput >= actualSunPower){
 				if (i != timespanYear) {
 					electricity = electricity +  actualSunPower*dailyHours*365*inverterEfficiency;
@@ -39,7 +40,7 @@ public class ElectricityCalculator implements ElectricityCalculationApi{
 	}
 
 	private void restrictInput(double dailySun, double inverterEfficiency,
-			double solarPanelEfficiency, double solarPowerOutput, double dailyHours,
+			double ageEfficiencyLoss, double solarPowerOutput, double dailyHours,
 			double timespan) throws IllegalArgumentException{
 		
 		if (dailySun < 0) {
@@ -48,9 +49,9 @@ public class ElectricityCalculator implements ElectricityCalculationApi{
 		}  else if (inverterEfficiency > 1 || inverterEfficiency < 0) {
 			throw new IllegalArgumentException("Invaild inverterEfficiency input, " +
 					"the inverter efficiency parameter shoulbe be between 0 and 1. ");
-		} else if (solarPanelEfficiency > 1 || solarPanelEfficiency < 0) {
-			throw new IllegalArgumentException("Invaild solarPanelEfficiency input," +
-					"the solar panel efficiency parameter shoulbe be between 0 and 1. "+solarPanelEfficiency);
+		} else if (ageEfficiencyLoss > 1 || ageEfficiencyLoss < 0) {
+			throw new IllegalArgumentException("Invaild ageEfficiencyLoss input," +
+					"the solar ageEfficiencyLoss parameter shoulbe be between 0 and 1. ");
 		} else if (solarPowerOutput < 0) {
 			throw new IllegalArgumentException("Invaild solarPowerOutPut input, " +
 					"the solar power output parameter shoulbe be more than zero. ");
